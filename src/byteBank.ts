ae7dd16b39c35588d6596bcf1e3041fb1ab33ec1
Bruno <bruno.m.machado@ba.estudante.senai.br>
2025-11-06 10:46:40 -0300

    type

--- src/byteBank.ts
+++ src/byteBank.ts
@@ -1,5 +1,6 @@
 let saldo = 3000;
 
+alert("Teste");
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
 if (elementoSaldo !== null) {
   elementoSaldo.textContent = saldo.toString();
