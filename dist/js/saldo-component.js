dd9cc6b018b354cd9594f8ea836501b742c7ea40
Bruno <bruno.m.machado@ba.estudante.senai.br>
2025-11-07 11:18:20 -0300

    Formatação do type

--- dist/js/saldo-component.js
+++ dist/js/saldo-component.js
@@ -1,6 +1,10 @@
 let saldo = 3000;
-alert("Teste");
 const elementoSaldo = document.querySelector(".saldo-valor .valor");
+const elementoDataAcesso = document.querySelector(".block-saldo time");
 if (elementoSaldo !== null) {
-    elementoSaldo.textContent = saldo.toString();
+    elementoSaldo.textContent = formatarMoeda(saldo);
+}
+if (elementoDataAcesso !== null) {
+    const dataAcesso = new Date();
+    elementoDataAcesso.textContent = formatarData(dataAcesso);
 }
